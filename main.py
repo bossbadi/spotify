3969324c15cb11e1ae8f5941ff64e4377c5ace13
Andy Tang <bossbadi123@gmail.com>
2025-06-16 16:20:46 -0400

    fix playlist syncing name conflict bug

--- main.py
+++ main.py
@@ -22,23 +22,31 @@ with open('playlists.json', 'r') as f:
 
 
 def sync_playlist(query):
-    for playlist in PLAYLISTS:
-        if not playlist.startswith(query):
-            continue
-
-        print(f'>>> Syncing playlist "{playlist}"')
-
-        # create playlist directory
-        os.makedirs(playlist, exist_ok=True)
-
-        if os.path.exists(f'{playlist}/.sync.spotdl'):
-            os.system(CMD_BASE_MAIN % (playlist))
-        else:
-            os.system(CMD_BASE_INIT % (PLAYLISTS[playlist], playlist))
-
-        break
-    else:
+    playlist = get_playlist_name(query)
+    if not playlist:
         print(f'Playlist "{query}" not found')
+        return
+
+    print(f'>>> Syncing playlist "{playlist}"')
+
+    # create playlist directory
+    os.makedirs(playlist, exist_ok=True)
+
+    if os.path.exists(f'{playlist}/.sync.spotdl'):
+        os.system(CMD_BASE_MAIN % (playlist))
+    else:
+        os.system(CMD_BASE_INIT % (PLAYLISTS[playlist], playlist))
+
+
+def get_playlist_name(query):
+    # first check if query is exact match
+    if query in PLAYLISTS:
+        return query
+
+    # then check if query is the start of a playlist name
+    for name in PLAYLISTS:
+        if name.startswith(query):
+            return name
 
 
 def main():
